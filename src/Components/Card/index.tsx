e75d826cca848df945e5254cad336ea0bbfa2b1a
willytec <alown666@gmail.com>
2022-04-13 11:24:47 +0100

    Complete card component, and style

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,9 +1,29 @@
+import {useState} from "react";
+import ContextMenu from "../ContextMenu";
 import "./style.scss";
 
 interface CardProps {
   children: React.ReactNode;
+  columnId: number;
+  cardId: number;
 }
 
-export default function Card({children}: CardProps): JSX.Element {
-  return <div className="card">{children}</div>;
+export default function Card({
+  children,
+  columnId,
+  cardId,
+}: CardProps): JSX.Element {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+  const [action, setAction] = useState(null);
+  return (
+    <div className="card">
+      <ContextMenu
+        visible={isMenuVisible}
+        handleVisible={setMenuVisible}
+        columnId={columnId}
+        cardId={cardId}
+      />
+      {children}
+    </div>
+  );
 }
